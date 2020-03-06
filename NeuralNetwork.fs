9684712377e2b91a2e7cea5cb47c95c03f303920
BearPro <bearpro@outlook.com>
2020-03-06 10:03:00 +0300

    Нейронная сеть - Добавлена возможность обработать уровни сеть с произвольными связями между уровнями.

--- NeuralNetwork.fs
+++ NeuralNetwork.fs
@@ -3,26 +3,52 @@ namespace MachineLearning
 open System
 open MachineLearning.MathNeuron
 
-module Network = 
-    type ConnectionMatrix = 
+module Network =
+    type ConnectionMatrix =
+    /// <summary>
+    /// Произвольная матрица соединений.
+    /// </summary>
     | Connections of (int * int) Set
+    /// <summary>
+    /// Означает, что для каждый выход нейрона слева связан со всеми нейронами
+    /// справа. Количество входов у каждого нейрона справа равно количеству
+    /// нейронов слева.
+    /// </summary>
     | Cross
+    /// <summary>
+    /// Означает, что для одного выхода нейрона слева существует один вход
+    /// нейрона справа.
+    /// Соответсвие входов и выходов определяется порядком их определения.
+    /// </summary>
     | OneToOne
 
     type Layer = Neuron list
-    let sensorLayer inputs = 
-        [for i in [0..inputs] do yield create [1.0] 1.0 ]
+    let sensorLayer inputs =
+        [for i in [1..inputs] do yield create [1.0] 1.0 ]
 
-    let connect layer1 connectionMatrix layer2 = 
+    let connect layer1 connectionMatrix layer2 =
         [(OneToOne, layer1); (connectionMatrix, layer2)]
 
-    let invokeLayer inputs (layer, connections) = 
+    let invokeLayer inputs (layer, connections) =
         match connections with
         | OneToOne -> List.map2 (fun n i -> MathNeuron.invoke [i] n) layer inputs
-        | _ -> failwith "Not implemented"
+        | Cross -> List.map (fun n -> MathNeuron.invoke inputs n) layer
+        | Connections c ->
+            c
+            |> Set.toList
+            |> List.groupBy (fun (inputIndex, neuronIndex) -> neuronIndex )
+            |> List.map (
+                fun (neuronIndex, inputIndexes) ->
+                    (layer.[neuronIndex],
+                     [for (_, i) in inputIndexes -> inputs.[i]]
+                    )
+             >> fun (neuron, inputs) -> neuron |> MathNeuron.invoke inputs)
 
-    let rec invoke inputs network = 
+
+    let rec invoke inputs network =
         match network with
-        | [(connections: ConnectionMatrix, layer: Layer)] -> invokeLayer inputs (layer, connections)
-        | (connections: ConnectionMatrix, layer: Layer) :: tail -> tail |> invoke (invoke inputs [(connections, layer)])
-        | _ -> failwith "Not implemented"
+        | [(connections: ConnectionMatrix, layer: Layer)] ->
+            invokeLayer inputs (layer, connections)
+        | (connections: ConnectionMatrix, layer: Layer) :: tail ->
+            tail |> invoke (invoke inputs [(connections, layer)])
+        | [] -> failwith "Невозможно выполнять вычисления в пустой нейронной сети."
