3a768b76fb2a4b6c60a5fad6c967bd9e0530a9c8
BearPro <bearpro@outlook.com>
2020-03-07 18:04:45 +0300

    Рефакторинг - Добавлены комментарии - Исправлены имена - Слегка оптимизирован алгоритм

--- HebbianLearning.fs
+++ HebbianLearning.fs
@@ -2,60 +2,75 @@ namespace MachineLearning
 
 module HebbianLearning =
 
+    /// <summary>
+    /// Представляет отдельную запись в таблице значений обучающего набора.
+    /// </summary>
     type LearningTarget =
         { Inputs: float list
           Output: float }
 
+    /// <summary>
+    /// Представляет таблицу значений для входов и выхода нейрона, по которой нейрон
+    /// будет обучаться.
+    /// </summary>
     type LearningTable = LearningTarget list
 
+    /// <summary>
+    /// Возвращает список без последнего элемента.
+    /// </summary>
+    let private skiplast source =
+        let array = source |> Array.ofList
+        let length = array.Length
+        array
+        |> Array.take (length - 1)
+        |> List.ofArray
+
+
+    /// <summary>
+    /// Выполняет модификацию весов нейрона таким образом, чтобы они соответсвовали данной
+    /// записи в таблице истинности.
+    /// </summary>
     let private fixWeights learningTarget neuron: Neuron =
         let newWeight weight input = weight + input * learningTarget.Output
         let newOffset weight = weight - learningTarget.Output
         { neuron with
               Weights =
-                  (List.map2 newWeight
-                       (neuron.Weights
-                        |> List.rev
-                        |> List.tail
-                        |> List.rev) learningTarget.Inputs)
+                  (List.map2 newWeight (skiplast neuron.Weights) learningTarget.Inputs)
                   @ [ newOffset (neuron.Weights |> List.last) ] }
 
-    (*
+(*
 лю
 *)
 
-    // let rec study learningTable (neuron: Neuron) =
-    //     learningTable
-    //     |> List.fold (fun neuron { Inputs = inputs; Output = expected } ->
-    //         if (neuron |> MathNeuron.Offset.invoke inputs) = expected then
-    //             neuron
-    //         else
-    //             study learningTable
-    //                 (neuron
-    //                  |> fixWeights
-    //                      { Inputs = inputs
-    //                        Output = expected })) neuron
-
+    /// <summary>
+    /// Представляет результат проверки соответствия.
+    /// </summary>
     type CorrectionCheckResult =
         | Error of index: int
         | Correct
 
+    /// <summary>
+    /// Проверяет соответсвие нейрона и таблицы истины. Возвращает
+    /// индекс первой записи, в которой обнаружено неосоответствие.
+    /// </summary>
     let private iscorrect learningTable neuron =
         learningTable
         |> Seq.ofList
         |> Seq.map (fun { Inputs = inputs; Output = expected } -> MathNeuron.Offset.invoke inputs neuron = expected)
         |> Seq.tryFindIndex ((=) false)
-        |> fun o ->
-            match o with
-            | Some index -> Error(index)
-            | None -> Correct
+        |> function
+        | Some index -> Error(index)
+        | None -> Correct
 
 
-
-    let rec studyfast learningTable neuron =
+    /// <summary>
+    /// Обучает нейрон по модели Хебба. Возвращает обученный нейрон,
+    /// с правильно настроенными весами.
+    /// </summary>
+    let rec study learningTable neuron =
         match iscorrect learningTable neuron with
         | Correct -> neuron
         | Error index ->
             neuron
             |> fixWeights learningTable.[index]
-            |> studyfast learningTable
+            |> study learningTable
