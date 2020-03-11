39970a6ee04e1ad9ec5db02fd4f8ce26fe838295
BearPro <bearpro@outlook.com>
2020-03-11 14:42:00 +0300

    Практика 2 - Добавлен третий слой нейронной сети. - Третий слой обучен на таблице истинности демультиплексора. - Изменён алгоритм представления реузльтата работы программы.

--- Practice/Practice2.fs
+++ Practice/Practice2.fs
@@ -52,37 +52,68 @@ module Practice2 =
                        Output =
                            if item = number then 1.0 else -1.0 })))
 
+    let bits integer =
+        let rec re =
+            function
+            | 0 -> [ 0 ]
+            | 1 -> [ 1 ]
+            | integer -> integer % 2 :: re (integer / 2)
+
+        let rec offset bits =
+            (List.length
+             >> (<=) 4
+             >> function
+             | true -> bits
+             | false -> offset (0 :: bits)) bits
+
+        integer
+        |> re
+        |> List.rev
+        |> offset
+
+    let integersStudySet =
+        let allBits =
+            [ 0 .. 9 ]
+            |> List.map
+                (bits
+                 >> List.map (function
+                     | 0 -> -1.0
+                     | 1 -> 1.0
+                     | _ -> failwith "Not valid value."))
+
+        let baseArray = System.Collections.Immutable.ImmutableArray.CreateRange([ 0 .. 9 ] |> List.map (fun _ -> -1.0))
+        let allInputs = [ 0 .. 9 ] |> List.map ((fun i -> baseArray.SetItem(i, 1.0)) >> List.ofSeq)
+        [ 0 .. 3 ]
+        |> List.map (fun neuronIndex ->
+            (neuronIndex,
+             List.map2 (fun inputs (bits: float list) ->
+                 { Inputs = inputs
+                   Output =
+                       match bits.[neuronIndex] with
+                       | -1.0 -> -1.0
+                       | 1.0 -> 1.0
+                       | _ -> failwith "Not valid value." }) allInputs allBits))
+
     /// <summary>
     /// Конструирует нейронную сеть, обучая второй уровень ("A-элементы") с помощью
     /// обучающего наора 'aLayerStudySet'. Возвращает обученную нейронную сеть.
     /// </summary>
-    let network (aLayerStudySet: (int * LearningTable) list): (ConnectionMatrix * Layer) list =
-        let ``input-s connection`` = Network.ConnectionMatrix.OneToOne
-
+    let network: (ConnectionMatrix * Layer) list =
         let sLayer =
             [ for i in [ 1 .. 15 ] do
                 yield MathNeuron.create [ 1.0 ] 1.0 ]
 
-        let ``s-a connection`` = Network.ConnectionMatrix.Cross
-
         let aLayer =
             [ for number in [ 0 .. 9 ] ->
-                printfn "Creating %d neuron" number
-                MathNeuron.create [ for _ in [ 0 .. 14 ] -> 0.0 ] 0.0
-                |> HebbianLearning.study
-                    (aLayerStudySet
-                     |> List.filter (fun (n, _) -> n = number)
-                     |> List.exactlyOne
-                     |> function
-                     | _, x -> x) (0.9, 0.0) ]
-
-        let ``a-r connection`` = Network.ConnectionMatrix.Cross
+                MathNeuron.create [ for _ in [ 0 .. 14 ] -> 0.0 ] 0.0 ]
 
         let rLayer =
             [ for _ in [ 0 .. 3 ] ->
                 MathNeuron.create [ for _ in [ 0 .. 9 ] -> 0.0 ] 0.0 ]
-        [ (``input-s connection``, sLayer)
-          (``s-a connection``, aLayer) ]
+
+        [ (OneToOne, sLayer)
+          (Cross, aLayer)
+          (Cross, rLayer) ]
 
     /// <summary>
     /// Простой пример обучения отдельного нейрона по модели Хебба.
@@ -99,7 +130,7 @@ module Practice2 =
                 Output = 1.0 } ]
 
         let neuron = MathNeuron.create [ 0.0; 0.0 ] 0.0
-        let smartNeuron = neuron |> HebbianLearning.study table (0.5, 0.0)
+        let smartNeuron = neuron |> HebbianLearning.studyNeuron table (0.5, 0.0)
         ()
 
     /// <summary>
@@ -110,17 +141,40 @@ module Practice2 =
     /// </summary>
     let test path network =
         let set = ImageSet.loadInputs path
+        let item =
+            function
+            | -1.0 -> 0
+            | 1.0 -> 1
+            | _ -> failwith "Fuck"
         for (name, content, inputs) in set do
-            printfn "\nName: %s\nContent:\n%s" name content
+            printfn "\nFile: %s\nContent:\n%s" name content
             let result = network |> Network.invoke inputs
-            printfn "Result is %d.\nOutputs:\n%A\n" (result |> List.findIndex ((<=) 0.0)) result
+            printfn "Result is %d.\nOutputs:\n%A\n"
+                (result
+                 |> List.rev
+                 |> Utils.enumerate
+                 |> List.sumBy (fun (i, v) -> int ((float 2 ** float i) * (float (item v)))))
+                (result |> List.map item)
+            ()
         ()
+
 
     /// <summary>
     /// Точка входа в модуль презентации результатов второго практического занятия.
     /// </summary>
     let main() =
         let studySet = ImageSet.load @"./Practice/Practice 2 study set"
-        let net = network studySet
+
+        let net =
+            network
+            |> function
+            | [ (w1, n1); (w2, n2); (w3, n3) ] ->
+                [ (w1, n1)
+                  (w2, n2 |> studyLayer studySet (1.0, 0.0))
+                  (w3, n3 |> studyLayer integersStudySet (1.0, 0.0)) ]
+            | [ (w1, n1); (w2, n2) ] ->
+                [ (w1, n1)
+                  (w2, n2 |> studyLayer studySet (1.0, 0.0)) ]
+            | _ -> failwith "Незапланированная структура нейронной сети."
         net |> test @"./Practice/Practice 2 study set"
         ()
