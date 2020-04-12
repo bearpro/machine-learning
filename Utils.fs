aa827b1706cb728a56f87de17bfa139516ed9c9c
BearPro <bearpro@outlook.com>
2020-04-12 14:03:03 +0300

    Добавлена функция times.

--- Utils.fs
+++ Utils.fs
@@ -9,3 +9,10 @@ module Utils =
             | [item] -> [(index, item)]
             | head::tail -> (index, head) :: re tail (index + 1)
         re list 0
+
+    /// Возвращает список из n значений value.
+    let times value n =
+        [for _ in 0..n -> value]
+
+    /// Оператор повторения заданного значения n раз.
+    let ( *| ) = times
