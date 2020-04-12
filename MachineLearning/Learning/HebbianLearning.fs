c03a401c001936288264190a44eb03e1999189a8
BearPro <bearpro@outlook.com>
2020-04-12 15:12:54 +0300

    Некоторые типы вынесены в пространство имён MachineLearning.Learning.

--- MachineLearning/Learning/HebbianLearning.fs
+++ MachineLearning/Learning/HebbianLearning.fs
@@ -3,26 +3,27 @@ namespace MachineLearning.Learning
 open MachineLearning
 open MachineLearning.MathNeuron
 
+/// <summary>
+/// Представляет отдельную запись в таблице значений обучающего набора.
+/// </summary>
+type LearningTarget =
+    { Inputs: float list
+      Output: float }
+
+/// <summary>
+/// Представляет таблицу значений для входов и выхода нейрона, по которой нейрон
+/// будет обучаться.
+/// </summary>
+type LearningTable = LearningTarget list
+
+/// <summary>
+/// Представляет результат проверки соответствия.
+/// </summary>
+type CorrectionCheckResult =
+    | Error of LearningTarget
+    | Correct
+
 module HebbianRule =
-    /// <summary>
-    /// Представляет отдельную запись в таблице значений обучающего набора.
-    /// </summary>
-    type LearningTarget =
-        { Inputs: float list
-          Output: float }
-
-    /// <summary>
-    /// Представляет таблицу значений для входов и выхода нейрона, по которой нейрон
-    /// будет обучаться.
-    /// </summary>
-    type LearningTable = LearningTarget list
-
-    /// <summary>
-    /// Представляет результат проверки соответствия.
-    /// </summary>
-    type CorrectionCheckResult =
-        | Error of LearningTarget
-        | Correct
 
     /// <summary>
     /// Выполняет модификацию весов нейрона таким образом, чтобы они соответсвовали данной
